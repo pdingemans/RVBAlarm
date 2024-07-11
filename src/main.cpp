8b803f04290ccd2b198ca5c2bd18fd430518eaa8
peter dingemans <p.dingemans>
2024-07-11 15:48:16 +0200

    adding watchdog timer

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,20 @@
 RVB alarm
 
 */
+#include <avr/wdt.h>
 #include "..\include\inputhandler.h"
 #include "..\include\analogInput.h"
 #include "..\include\digitalInput.h"
 // definitions for analoginputs
 constexpr uint8_t sensorPinAR = A1;  // select the input pin for Audio Right
 constexpr uint8_t sensorPinAL = A0;  // select the input pin for Audio Left
-constexpr uint16_t threshold = 2;   // 25 seems to work well with the 25 samples and wavs
+constexpr uint16_t threshold = 2;    // 25 seems to work well with the 25 samples and wavs
 constexpr uint16_t thresholdLow = 0; // hysterese...
 
 // increase high edge detection to 250 msec. 5 msec was too short..
 constexpr uint8_t timeBetweenPolls = 5; // was 1
-constexpr uint8_t nrHighPolls = 50;  // amount of polls the signal needs to be active before going high  , was 5
-constexpr uint16_t inactiveTime = 10; // amount of polls the signal needs to be inactive before going low
+constexpr uint8_t nrHighPolls = 50;     // amount of polls the signal needs to be active before going high  , was 5
+constexpr uint16_t inactiveTime = 10;   // amount of polls the signal needs to be inactive before going low
 
 // definitions for digital inputs
 constexpr uint8_t pinSet = 3;
@@ -40,7 +41,7 @@ void setup()
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   Serial.println("-----  resetting ------");
-
+  wdt_enable(WDTO_250MS);
 }
 
 void loop()
@@ -52,29 +53,6 @@ void loop()
   manualSet.poll();
   myHandler.handle();
 
-  // Serial.print(sensorValueAL);
-  // Serial.print("     ");
-  // Serial.println(sensorValueAR);
-  // if the analog value is high enough, turn on the LED:
-  /* if (sensorValueAR > threshold) {
-    digitalWrite(ledPin, HIGH);
-    digitalWrite(sirene, HIGH);
-    Serial.println("Vliegverbod Actief");
-    Serial.println("Sirene aan");
-    delay(1000);
-  } else {
-      if (sirenestatus=1) {
-      digitalWrite(sirene, LOW);
-    Serial.println("Sirene uit");
-      }
-      else { }
-    }
-  if (sensorValueAL > threshold) {
-    digitalWrite(ledPin, LOW);
-    Serial.println("Vliegverbod Opgeheven");
-    delay(1000);
-  } else { }
-  // stop the program for for 100 milliseconds:
-  delay(100);
-  */
+  // Reset the watchdog timer
+  wdt_reset();
 }
